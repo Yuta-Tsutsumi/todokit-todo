1f74491d2e8322e9f74c77ea7ccc38456c59355a
Yuta-Tsutsumi <watagasi.789@gmail.com>
2022-03-30 00:52:58 +0900

    エラー

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styles from "./App.module.scss";
+import Header from "./components/header/Header";
 
 export const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
