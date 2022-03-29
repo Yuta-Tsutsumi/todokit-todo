aae61e5fedcf271658c2c590b7d82478e7781537
Yuta-Tsutsumi <watagasi.789@gmail.com>
2022-03-30 00:52:04 +0900

    エラー

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import styles from "./Header.module.scss";
+
+export const Header: React.FC = () => {
+  return (
+    <div className={styles.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={styles.root}>
+            News
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
