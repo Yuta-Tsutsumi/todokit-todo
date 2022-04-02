31e11715e533508f4a7c623abb81371922a5f2d0
Yuta-Tsutsumi <watagasi.789@gmail.com>
2022-04-02 09:57:09 +0900

    taskFormの追加

---
+++ src/features/task/taskForm/TaskForm.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import TextField from "@material-ui/core/TextField";
+import styles from "./TaskForm.module.scss";
+
+//typeでInputs変数を用意する
+type Inputs = {
+  //taskTitleをstringに型を決める。（文字列）
+  taskTitle: string;
+};
+
+export const TaskForm: React.FC = () => {
+  //react-hook-formで用意されている関数で、使いたい関数を定義
+  const { register, handleSubmit, reset } = useForm();
+  //(data: Inputs)←taskTitleがdata型で渡される。
+  //register（レジスター）として登録されたものに対してがdataとしてhandleCreateにわたされる
+  const handleCreate = (data: Inputs) => {
+    console.log(data);
+  };
+  return (
+    <div className={styles.root}>
+      <form
+        //handleSubmit（ハンドルサブミット）関数の中に16行目で定義した関数を渡す。
+        //そうする事でevent.target.valueと定義しなくてもreact-hook-formでフォームでの入力・編集の処理を簡単に実装できる。
+        onSubmit={handleSubmit(handleCreate)}
+        className={styles.form}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="outlined-basic"
+          label="New Task"
+          variant="outlined"
+          //material-uiにreact-hook-formを追加する際に必要
+          inputRef={register}
+          //dataの中にtaskTitleといったものが渡される
+          name="taskTitle"
+          className={styles.text_field}
+        />
+      </form>
+    </div>
+  );
+};
+
+{
+  /* <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Box>; */
+}
+
+//↑Boxが毎回ついてくるこれなに
