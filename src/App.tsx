dd47f4ec36399439a792f31009f1d39b3fe11aea
Yuta-Tsutsumi <watagasi.789@gmail.com>
2022-04-02 09:58:08 +0900

    TaskFormコンポーネントの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./App.module.scss";
 import { Header } from "./components/header/Header";
+import { TaskForm } from "./features/task/taskForm/TaskForm";
 
 export const App: React.FC = () => {
   return (
-    <div>
-      <Header />
+    <div className={styles.root}>
+      <div className={styles.wrapper}>
+        <Header />
+        <TaskForm />
+      </div>
     </div>
   );
 };
