d52b01842dc9a7c1d4495486312bd02038645580
Yuta-Tsutsumi <watagasi.789@gmail.com>
2022-04-19 01:46:32 +0900

    useSelectorのインポート

--- src/features/task/taskSlice.ts
+++ src/features/task/taskSlice.ts
@@ -1,5 +1,6 @@
 import { StarRate } from "@material-ui/icons";
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { useSelector } from "react-redux";
 import { RootState, AppThunk } from "../../app/store";
 // import { fetchCount } from "./counterAPI";
 
@@ -42,21 +43,7 @@ export const taskSlice = createSlice({
 
 export const { createTask } = taskSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectTask = (state: RootState): TaskState["tasks"] =>
   state.task.tasks;
-
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
-// export const incrementIfOdd =
-//   (amount: number): AppThunk =>
-//   (dispatch, getState) => {
-//     const currentValue = selectCount(getState());
-//     if (currentValue % 2 === 1) {
-//       dispatch(incrementByAmount(amount));
-//     }
-//   };
 
 export default taskSlice.reducer;
