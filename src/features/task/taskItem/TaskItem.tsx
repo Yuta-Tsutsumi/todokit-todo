750dba63b5a21fb88d985e7484050fedc210c375
Yuta-Tsutsumi <watagasi.789@gmail.com>
2022-06-04 10:48:21 +0900

    モーダルの編集ボタンの追加

--- src/features/task/taskItem/TaskItem.tsx
+++ src/features/task/taskItem/TaskItem.tsx
@@ -4,7 +4,13 @@ import Checkbox from "@material-ui/core/Checkbox";
 import EventNoteIcon from "@material-ui/icons/EventNote";
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
-import { handleModalOpen, selectIsModalOpen } from "../taskSlice";
+import {
+  deleteTask,
+  selectTask,
+  completeTask,
+  handleModalOpen,
+  selectIsModalOpen,
+} from "../taskSlice";
 import Modal from "@material-ui/core//Modal";
 import { TaskForm } from "../taskForm/TaskForm";
 import styles from "./TaskItem.module.scss";
@@ -17,6 +23,7 @@ export const TaskItem: React.FC<PropTypes> = ({ task }) => {
   const isModalOpen = useSelector(selectIsModalOpen);
   const dispatch = useDispatch();
   const handleOpen = () => {
+    dispatch(selectTask(task));
     dispatch(handleModalOpen(true));
   };
   const handleClose = () => {
@@ -32,14 +39,15 @@ export const TaskItem: React.FC<PropTypes> = ({ task }) => {
         <Checkbox
           //チェックボックスの記述
           checked={task.completed}
-          onClick={() => console.log(`check ${task.id}`)}
+          onClick={() => dispatch(completeTask(task))}
           className={styles.checkbox}
         />
         <button onClick={handleOpen} className={styles.edit_button}>
           <EditIcon className={styles.icon} />
         </button>
+        {/* 削除ボタン */}
         <button
-          onClick={() => console.log(`delete ${task.id}`)}
+          onClick={() => dispatch(deleteTask(task))}
           className={styles.delete_button}
         >
           <DeleteIcon className={styles.icon} />
@@ -48,22 +56,7 @@ export const TaskItem: React.FC<PropTypes> = ({ task }) => {
       <Modal open={isModalOpen} onClose={handleClose} className={styles.modal}>
         <div className={styles.modal_content}>
           <div className={styles.modal_title}>Edit</div>
-          <input
-            //パラメータ（エラー）
-            id="outlined-basic"
-            placeholder="Edit Task"
-            type="text"
-            name="taskTitle"
-            className={styles.text_field}
-          />
-          <div className={styles.button_wrapper}>
-            <button type="submit" className={styles.submit_button}>
-              submit
-            </button>
-            <button type="button" className={styles.cancel_button}>
-              submit
-            </button>
-          </div>
+          <TaskForm edit />
         </div>
       </Modal>
     </div>
